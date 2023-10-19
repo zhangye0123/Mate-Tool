da375c3aa29a582b8740144dfe989b09f596ccd4
张烨 <ye.zhang@appshahe.com>
2023-10-19 20:33:25 +0800

    add：通过深层遍历的方式获取Script对象

--- propertyResetterJSON.py
+++ propertyResetterJSON.py
@@ -5,70 +5,70 @@ import os
 file_path = "026NewLevel.level"
 # 027Level文件
 file_path2 = "NewLevel.level"
+replace_data_dict = {}
+static_replace_data_dict = {}
 
-replace_data_dict = dict()
-static_replace_data_dict = dict()
+
+def traverse_json(obj):
+    if isinstance(obj, dict):
+        if "Script" in obj:
+            script = obj["Script"]
+            if script:
+                script_component = script.get("ScriptComponent")
+                if script_component:
+                    for k, v in script_component.items():
+                        replace_data_dict[k] = v
+                static_script_component = script.get("StaticScriptComponent")
+                if static_script_component:
+                    for k, v in static_script_component.items():
+                        static_replace_data_dict[k] = v
+
+        for value in obj.values():
+            traverse_json(value)
+    elif isinstance(obj, list):
+        for item in obj:
+            traverse_json(item)
+
+def write_traverse_json(obj):
+    if isinstance(obj, dict):
+        if "Script" in obj:
+            script = obj["Script"]
+            if script:
+                script_component = script.get("ScriptComponent")
+                if script_component:
+                    for k, v in script_component.items():
+                        script_component[k] = replace_data_dict.get(k, v)
+                static_script_component = script.get("StaticScriptComponent")
+                if static_script_component:
+                    for k, v in static_script_component.items():
+                        static_script_component[k] = static_replace_data_dict.get(k, v)
+
+        for value in obj.values():
+            write_traverse_json(value)
+    elif isinstance(obj, list):
+        for item in obj:
+            write_traverse_json(item)
+    
+    return obj
 
 
 def main():
     with open(file_path, 'r', encoding='utf-8-sig') as f:
         data = json.load(f)
-        scene_list = data["Scene"]
-        for scene_item in scene_list:
-            script = scene_item.get("Script")
-            # 判空
-            if script:
-                script_component = script.get("ScriptComponent")
-                # 去除空数据
-                if script_component:
-                    # 遍历所有guid对应的json对象（key为guid）
-                    for k, v in script_component.items():
-                        # 跳过包含“相机”的项
-                        # if v.get("cameraMode"):
-                        #     continue
-                        replace_data_dict[k] = v
-                # print(script_component)
-                static_script_component = script.get("StaticScriptComponent")
-                # 去除空数据
-                if static_script_component:
-                    # 遍历所有guid对应的json对象（key为guid）
-                    for k, v in static_script_component.items():
-                        # 跳过包含“相机”的项
-                        # if v.get("cameraMode"):
-                        #     continue
-                        static_replace_data_dict[k] = v
-                # print(script_component)
+        traverse_json(data)
 
-        f.close()
-    out_str = ""
     with open(file_path2, 'r+', encoding='utf-8-sig') as f:
         data = json.load(f)
-        scene_list = data["Scene"]
-        for scene_item in scene_list:
-            script = scene_item.get("Script")
-            # 判空
-            if script:
-                script_component = script.get("ScriptComponent")
-                if script_component:
-                    # 遍历所有guid对应的json对象（key为guid）
-                    for k, v in script_component.items():
-                        script_component[k] = replace_data_dict[k]
-                static_script_component = script.get("StaticScriptComponent")
-                if static_script_component:
-                    # 遍历所有guid对应的json对象（key为guid）
-                    for k, v in static_script_component.items():
-                        static_script_component[k] = static_replace_data_dict[k]
+        # traverse_json(data)
+        data = write_traverse_json(data)
 
-        # out_str = json.dumps(data)
-        # f.close()
+        out_str = json.dumps(data)
+        f.seek(0)
+        f.write(out_str)
+        f.truncate()
 
-        with open(file_path2, 'w', encoding='utf-8') as f:
-            # f.write(out_str)
-            json.dump(data, f)
-        f.close()
+    print("脚本修改完成！")
 
 
 if __name__ == '__main__':
     main()
-
-os.system("pause")
