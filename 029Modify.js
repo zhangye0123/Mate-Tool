315640667eaf517f7faa99c76d8c4c3b387deacf
张烨 <ye.zhang@appshahe.com>
2023-12-21 16:18:42 +0800

    add：029补充提交

--- 029Modify.js
+++ 029Modify.js
@@ -1,26 +1,65 @@
 const fs = require('fs');
 const path = require('path');
 
+
 // 定义要搜索的文件夹和文件名
-const folderPath = path.join(__dirname, 'JavaScripts/Modified027Editor');
-const fileName = 'ModifiedPlayer.ts';
+const targetFiles = [
+    { subFolder: 'JavaScripts/Modified027Editor', file: 'ModifiedPlayer.ts' },
+    { subFolder: 'JavaScripts', file: 'sevenAnimation.ts' }
+];
 
-// 构建文件的完整路径
-const filePath = path.join(folderPath, fileName);
+function changeFile(filePath) {
+    console.error("filePath", filePath);
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        if (err) {
+            console.log(`Error reading file: ${filePath}`);
+            return;
+        }
 
-// 读取文件内容
-fs.readFile(filePath, 'utf8', function (err, data) {
-    if (err) {
-        return console.log("this file is not exist");
-    }
+        // 替换文本
+        let result = data.replace(/return stance;/g, 'return stance as any;');
+        result = result.replace(/return ani;/g, 'return ani as any;');
+        result = result.replace(/return anim;/g, 'return anim as any;');
+        result = result.replace(/ as RpcAnimation/g, ' as any');
 
-    // 替换文本
-    let result = data.replace(/return stance;/g, 'return stance as any;');
-    result = result.replace(/return ani;/g, 'return ani as any;');
-    result = result.replace(/return anim;/g, 'return anim as any;');
-    
-    // 写回文件
-    fs.writeFile(filePath, result, 'utf8', function (err) {
-        if (err) return console.log(err);
+        // 写回文件
+        fs.writeFile(filePath, result, 'utf8', function (err) {
+            if (err) {
+                console.log(`Error writing file: ${filePath}`);
+                return;
+            }
+            console.log(`File updated: ${filePath}`);
+        });
     });
-});
+}
+
+function processSubdirectories(basePath) {
+    fs.readdir(basePath, { withFileTypes: true }, (err, dirents) => {
+
+        if (err) {
+            console.error('Error reading directory:', basePath, dirent.name);
+            return;
+        }
+
+        dirents.forEach(dirent => {
+            if (dirent.isDirectory()) {
+                // 遍历每个子文件夹
+                targetFiles.forEach(({ subFolder, file }) => {
+                    const fullFolderPath = path.join(basePath, dirent.name, subFolder);
+                    const fullPath = path.join(fullFolderPath, file);
+                    if (fs.existsSync(fullPath)) {
+                        changeFile(fullPath);
+                    }
+                });
+            }
+        });
+    });
+}
+
+
+const currentDir = process.cwd();
+//获取当前脚本所在目录的上一级目录
+const rootPat1 = path.dirname(currentDir);
+processSubdirectories(rootPat1);
+
+
--- 029ModifyPy.py
+++ 029ModifyPy.py
@@ -4,21 +4,30 @@ import os
 folder_path = 'JavaScripts/Modified027Editor'
 file_name = 'ModifiedPlayer.ts'
 
+folder_path1 = 'JavaScripts'
+file_name1 = 'sevenAnimation.ts'
+
 # 构建文件的完整路径
 file_path = os.path.join(os.getcwd(), folder_path, file_name)
+file_path1 = os.path.join(os.getcwd(), folder_path1, file_name1)
 
-# 读取文件内容并替换文本
-try:
-    with open(file_path, 'r', encoding='utf-8') as file:
+# 将下述功能写成函数，并调用
+def changeTxt(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
         content = file.read()
         content = content.replace('return stance;', 'return stance as any;')
         content = content.replace('return ani;', 'return ani as any;')
         content = content.replace('return anim;', 'return anim as any;')
+        content = content.replace(' as RpcAnimation', ' as any')
 
     # 写回文件
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(filepath, 'w', encoding='utf-8') as file:
         file.write(content)
 
+# 读取文件内容并替换文本
+try:
+    changeTxt(file_path)
+    changeTxt(file_path1)
     print("文件修改成功")
 except IOError as e:
     print(f"发生错误: {e}")
